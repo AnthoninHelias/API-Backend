7f2b24da86664473541b3adcffbb76b741861dec
Anthonin Helias <heliasantho@yahoo.fr>
2024-02-27 12:24:19 +0100

    connection réussi

--- src/server.js
+++ src/server.js
@@ -1,6 +1,34 @@
-const router = require('express').Router();
-const swaggerUi = require('swagger-ui-express');
-const getConfig = require('../middleware/swagger.middleware');
-router.use('/doc', swaggerUi.serve);
-router.get('/doc', swaggerUi.setup(getConfigSwagger.swaggerOptions, getConfigSwagger.swaggerSortByHTTPRequest));
-module.exports = router;
+const router = require('express').Router()
+const swaggerUI = require('swagger-ui-express')
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+
+const getConfigSwagger = require('./middleware/swagger.js')
+
+dotenv.config()
+
+const {Client} = require('pg')
+
+const client = new Client({
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    port: process.env.PORT
+})
+
+client.connect()
+console.log("Connecté")
+
+const server = app.listen(8085, function () {
+    const host = server.address().address
+    const port = server.address().port
+
+    console.log('Example app listening at http://localhost:8085', host, port)
+})
+
+app.use('/doc', swaggerUI.serve)
+app.get('/doc', swaggerUI.setup(getConfigSwagger.swaggerOptions, getConfigSwagger.swaggerSortByHTTPRequest))
+
+module.exports = router
